25db9fb66bc45d8d5e1618b972b43d42406d10f3
Bohdan Zhykhor <bohdanzhyhor@gmail.com>
2024-09-02 23:02:29 -0400

    finished header and footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    fontFamily: {
+      "Roboto-Flex": ["Roboto Flex"],
+      "Roboto-Condensed": ["Roboto Condensed"],
+      "Bebas-Neue": ["Bebas Neue"],
+    },
   },
   plugins: [],
 };
