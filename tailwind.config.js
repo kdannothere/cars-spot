1da11d5d50b1d843fee2b97cc67b392091e0cf4e
Bohdan Zhykhor <bohdanzhyhor@gmail.com>
2024-09-07 14:32:06 -0400

    completed version 1

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,16 @@ module.exports = {
       "Roboto-Condensed": ["Roboto Condensed"],
       "Bebas-Neue": ["Bebas Neue"],
     },
+    screens: {
+      'tablet': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'laptop': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'desktop': '1440px',
+      // => @media (min-width: 1280px) { ... }
+    },
   },
   plugins: [],
 };
